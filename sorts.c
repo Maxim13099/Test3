d3542788f2c2b59e68173b47b5c07359a329d7b0
Maxim13099 <122614879+Maxim13099@users.noreply.github.com>
2023-02-17 16:59:51 +0300

    Create sorts.c

---
+++ sorts.c
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <chrono>
+#include <cstring>
+using namespace std;
+
+int main()
+{ 
+    int arsize[] = 100
+    int *ar = new int[arsize]
+    cout<<arsize<<"/t"
+    randomFilling(ar, arsize, 0,999)
+    
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    start = std::choro::high_resolution_clock::now();
+    
+int mergeSort(int* ar, int size) {
+    if(size <= 1) {
+        return -1;
+    }
+    int lSize = size/2;
+    int rSize = size - lSize;
+    mergeSort(ar, lSize);
+    mergeSort(ar + lSize, rSize);
+    int* sAr = new int[size];
+    merge(ar, lSize, ar + lSize, rSize, sAr);
+    std::memcpy(ar, sAr, sizeof(int) * size);
+    delete [] sAr;
+    return -1;
+}
+
+    
+    std::chrono::time_point<std::chrono::high_resolution_clock:: end;
+    end = std::chrono::high_resolution_clock::now()
+    std::chrono::duration<double> dif = end - start;
+    std::cout << diff.count() << std::endl;
+    return 0;
+}
+    
